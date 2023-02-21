bebba4248a270fca29a80e275b268f486017ae9d
Dantolos <45291569+Dantolos@users.noreply.github.com>
2023-02-21 22:57:54 +0100

    updatetest

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 export default function Sidebar(){
      return (
           <sidebar className={styles.sidebar}>
+               <h4>SIDEBAR</h4>
                <Link  href="/classes"passhref>
                     <h3>Classes</h3>
                </Link> 
