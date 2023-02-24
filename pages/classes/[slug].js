b17c045b8e41cbee794164a81b161335b7fc2ccd
Dantolos <45291569+Dantolos@users.noreply.github.com>
2023-02-24 10:16:02 +0100

    fallback true

--- pages/classes/[slug].js
+++ pages/classes/[slug].js
@@ -43,7 +43,6 @@ export const getStaticPaths = async () => {
                     slug: post.slug,
                },
           })),
-          revalidate: 10,
-          fallback: false,
+          fallback: true,
      };
 };
